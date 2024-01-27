0bf740b0c28ee7ce516433b72b99e88a9c557ab5
AliAais <aaisali228@gmail.com>
2024-01-27 09:17:45 -0800

    fixed responsiveness

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -22,32 +22,32 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className='py-6 -z-50 overflow-hidden'>
-      <div className='swiper-container w-[60vw] mx-auto h-[60vh]'>
+    <div className='py-6 overflow-hidden'>
+      <div className='swiper-container w-[95vw] md:w-[65vw] mx-auto h-[65vh]'>
         <div className='swiper-wrapper'>
           <div className='swiper-slide'>
             <Link to="/women">
-            <img src={Image1} className='w-full h-full object-cover' alt="Pic1" />
+            <img src={Image1} className='w-full h-full object-fit ' alt="Pic1" />
             </Link>
           </div>
           <div className='swiper-slide'>
             <Link to="/men">
-            <img src={Image2} className='w-full h-full object-cover' alt="Pic2" />
+            <img src={Image2} className='w-full h-full object-fit' alt="Pic2" />
             </Link>
           </div>
           <div className='swiper-slide'>
             <Link to="/children">
-            <img src={Image3} className='w-full h-full object-cover' alt="Pic3" />
+            <img src={Image3} className='w-full h-full object-fit' alt="Pic3" />
             </Link>
           </div>
           <div className='swiper-slide'>
             <Link to="women">
-            <img src={Image4} className='w-full h-full object-cover' alt="Pic3" />
+            <img src={Image4} className='w-full h-full object-fit' alt="Pic3" />
             </Link>
           </div>
           <div className='swiper-slide'>
             <Link to="men">
-            <img src={Image5} className='w-full h-full object-cover' alt="Pic3" />
+            <img src={Image5} className='w-full h-full object-fit' alt="Pic3" />
             </Link>
           </div>
         </div>
