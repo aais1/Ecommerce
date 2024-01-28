20f2f07ed7ad513fec9e7e09df47e3054aa89f1b
AliAais <aaisali228@gmail.com>
2024-01-28 04:35:34 -0800

    Refactor Product component layout and styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -33,14 +33,14 @@ const Product = () => {
     return (
       <>
         <div className='w-[80vw] mx-auto my-8 p-12 border'>
-          <span><Skeleton /></span>
-          <h3><Skeleton /></h3>
+          <span className='mb-5 p-2'><Skeleton /></span>
+          <h3 className='p-2'><Skeleton count={2} /></h3>
           <div>
             {
-              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-14'>
-                <Skeleton count={3} width={300} height={300} />
-                <Skeleton count={3} width={300} height={300} />
-                <Skeleton count={3} width={300} height={300} />
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-2 mt-14'>
+                <Skeleton count={1} width={300} height={300} />
+                <Skeleton count={1} width={300} height={300} />
+                <Skeleton count={1} width={300} height={300} />
               </div>
             }
           </div>
@@ -55,19 +55,19 @@ const Product = () => {
   }
 
   return (
-    <div className='md:border md:p-12  w-[80vw] mx-auto my-8  min-h-[400px]'>
-      <div className='flex justify-between'>
+    <div className='md:border md:p-12  w-[90vw] mx-auto my-8  min-h-[400px]'>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+      <div className=' flex flex-col'>
         <span className='font-bold text-xl md:text-3xl mb-5 p-2 '>{data.title}</span>
-        <span className='text-2xl text-red-500 font-bold'>Price {data.price}$</span>
+        <span className='text-2xl text-red-500 font-bold p-2'>Price: {data.price}$</span>
+         <h3 className='text-md p-2 '>{data.description}</h3>
       </div>
-    
-    <h3 className='text-md p-2 '>{data.description}</h3>
     <div className='flex justify-center items-center'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-14'>
+      <div className='grid grid-cols-2 gap-3'>
           {
               data.images.map((image)=>{
                    return (
-                    <div className='w-[280px] h-[320] md:w-[300px] md:h-[350px] rounded-md mt-6 overflow-hidden' key={image}>
+                    <div className='w-[200px] h-[250] md:w-[280px] md:h-[300px] rounded-md mt-6 overflow-hidden' key={image}>
                     <img  src={image} className='w-[100%] h-[100%] rounded-md overflow-hidden duration-200 hover:scale-110' alt={data.title} />
                     </div>
                   )
@@ -79,6 +79,7 @@ const Product = () => {
     <div className='mt-8 grid grid-cols-2 items-end'>
       {/* <button>Place your button content here</button> */}
     </div>
+  </div>
   </div>
   );
 };
